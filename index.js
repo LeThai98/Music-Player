05bf6386fefec6bbe0eccfd1125376edf978a2b9
lethai <hongthaiit1998@gmail.com>
2024-03-02 15:57:17 +0700

    manipulate  HTMLAudioElement, HTMLInputElement and Using setInterval to update value by time

---
+++ index.js
@@ -0,0 +1,39 @@
+// audio element is represented by an object is an instanceof HTMLAudioElement -> HTMLMediaElement -> HTMLElement
+// input element is represented by an object is an instanceof HTMLInputElement -> HTMLElement
+
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+// song.onloadedmetadata = function(){
+//     progress.max = song.duration;
+//     progress.value = song.currentTime;
+// }
+
+song.addEventListener("loadedmetadata", (event)=>{
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+})
+
+function playPause(){
+    if(ctrlIcon.classList.contains('fa-pause')){
+        song.pause();
+        ctrlIcon.classList.remove('fa-pause');
+        ctrlIcon.classList.add('fa-play');
+    }
+    else{
+        song.play();
+        song.addEventListener('play', function() {
+            console.log('Audio is now playing');
+        });
+        ctrlIcon.classList.add('fa-pause');
+        ctrlIcon.classList.remove('fa-play');
+    }
+}
+
+if(song.play()){
+    // setInterval is used to update value by time
+    setInterval(() =>{
+        progress.value = song.currentTime;
+    }, 500);
+}
